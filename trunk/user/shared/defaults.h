85441327d6ceb4d23b7019eb9b66c514b5682b57
aptx-yc <78417944+aptx-yc@users.noreply.github.com>
2023-02-03 14:55:49 +0800

    Update defaults.h

--- trunk/user/shared/defaults.h
+++ trunk/user/shared/defaults.h
@@ -37,10 +37,10 @@
 
 #define DEF_WLAN_2G_CC		"CN"
 #define DEF_WLAN_5G_CC		"US"
-#define DEF_WLAN_2G_SSID  "Cube"
-#define DEF_WLAN_5G_SSID  "Cube-5G"
-#define DEF_WLAN_2G_GSSID "Cube-Guest"
-#define DEF_WLAN_5G_GSSID "Cube-Guest-5G"
+#define DEF_WLAN_2G_SSID		"Cube"
+#define DEF_WLAN_5G_SSID		"Cube-5G"
+#define DEF_WLAN_2G_GSSID		"Cube-Guest"
+#define DEF_WLAN_5G_GSSID		"Cube-Guest-5G"
 #define DEF_WLAN_2G_PSK		"myj2q76b"
 #define DEF_WLAN_5G_PSK		"myj2q76b"
 
